2ab3743cecf9494c4d2c9f840fe4c24967a5d594
Andrey <ilynych0810@gmail.com>
2025-06-30 21:52:48 +0200

    Connected css

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function business_agency_enqueue_assets() {
-    wp_enqueue_style('business-agency-style', get_stylesheet_uri());
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'business_agency_enqueue_assets');
 
