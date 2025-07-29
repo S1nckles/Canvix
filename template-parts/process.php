3d53ed516868e42a3157dba09565c488a6607bc7
Andrey <ilynych0810@gmail.com>
2025-07-29 22:05:28 +0200

    Added Process Section

--- template-parts/process.php
+++ template-parts/process.php
@@ -1,17 +1,29 @@
+<?php sanitize_svg_colors($svg);
+?>
+
+
 <section class="process">
   <div class="container">
-    <h2><?= esc_html(get_field('process_title')) ?></h2>
+    <div class="process__inner">
+      <div class="process__title-wrapper">
+        <span class="section__subtitle">Process</span>
+        <h2 class="process__title"> <?= esc_html(get_field('process_title')) ?></h2>
+      </div>
 
-    <div class="process-steps">
-      <?php for ($i = 1; $i <= 4; $i++): 
-        $step = get_field("step_$i");
-        if ($step): ?>
-          <div class="process-step">
-            <div class="step-number"><?= $i ?></div>
-            <h3><?= esc_html($step['title']) ?></h3>
-            <p><?= esc_html($step['description']) ?></p>
-          </div>
-      <?php endif; endfor; ?>
+      <div class="process__steps">
+        <?php for ($i = 1; $i <= 4; $i++): 
+          $step = get_field("step_$i");
+          if ($step): ?>
+            <div class="process__step">
+              <div class="step__wrapper">
+                <div class="step__icon"><?= sanitize_svg_colors($step['icon_inline']) ?></div>
+                <h3 class="step__title"><?= esc_html($step['title']) ?></h3>
+              </div>
+              <p class="step__desc"><?= esc_html($step['description']) ?></p>
+            </div>
+        <?php endif; endfor; ?>
+      </div>
+
     </div>
   </div>
 </section>
