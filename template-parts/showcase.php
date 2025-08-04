b38ab32ecd765055ff336819b06e1eb3414f3b59
Andrey <ilynych0810@gmail.com>
2025-08-04 20:22:06 +0200

    Added ShowCase Section

--- template-parts/showcase.php
+++ template-parts/showcase.php
@@ -1,8 +1,7 @@
 <section class="showcase">
   <div class="container">
-    <h2><?= esc_html(get_field('showcase_title')) ?></h2>
-
-    <div class="project-cards">
+    <h2 class="showcase__title"><?= esc_html(get_field('showcase_title')) ?></h2>
+    <div class="showcase__items">
       <?php
       $projects = new WP_Query([
         'post_type' => 'project',
@@ -10,17 +9,20 @@
       ]);
       if ($projects->have_posts()):
         while ($projects->have_posts()): $projects->the_post(); ?>
-          <div class="project-card">
+          <div class="showcase__item">
             <?php if (has_post_thumbnail()): ?>
-              <a href="<?= get_permalink(); ?>">
+              <a class="showcase__item-image" href="<?= get_permalink(); ?>">
                 <?= get_the_post_thumbnail(null, 'medium'); ?>
               </a>
-            <?php endif; ?>
+              <?php endif; ?>
 
-            <h3><a href="<?= get_permalink(); ?>"><?= get_the_title(); ?></a></h3>
-            <p><?= wp_trim_words(get_the_content(), 20); ?></p>
+              <h3 class="showcase__item-title"><a href="<?= get_permalink(); ?>"><?= get_the_title(); ?></a></h3>
+            <p class="showcase__item-desc"><?= wp_trim_words(get_the_content(), 20); ?></p>
           </div>
-      <?php endwhile; wp_reset_postdata(); endif; ?>
+          <?php endwhile; wp_reset_postdata(); endif; ?>
+          <a href="#" class="btn btn-link showcase__btn">
+              Start your Free Trial
+            </a>
     </div>
   </div>
 </section>
