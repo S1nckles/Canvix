01e131209fca07978b233261d2af6252a70a5c15
Andrey <ilynych0810@gmail.com>
2025-08-20 21:55:01 +0200

    Update cta-swiper.js

--- assets/js/cta-swiper.js
+++ assets/js/cta-swiper.js
@@ -1,12 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const swiper = new Swiper('.cta-swiper', {
-      loop: true,
-      autoHeight: true, 
-      slidesPerView: 1,
-      spaceBetween: 20,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  const el = document.querySelector('.cta-swiper');
+  if (!el) return;
+
+  new Swiper(el, {
+    slidesPerView: 1,
+    loop: true,
+    autoHeight: true,
+    navigation: {
+      nextEl: '.testimonial .next',
+      prevEl: '.testimonial .prev',
+    },
+    a11y: true,
+  });
 });
