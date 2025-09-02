8208c09eb303192bb4816c2fe5af6567092591f5
Andrey <ilynych0810@gmail.com>
2025-09-02 12:20:08 +0200

    Added About-Banner Section

---
+++ template-parts/about/about-banner.php
@@ -0,0 +1,8 @@
+<section class="about-banner">
+    <div class="container">
+        <div class="about-banner__inner">
+            <h2 class="about-banner__title">Enough talk, let’s get to work</h2>
+            <a href="#" class="about-banner__btn btn">Get in touch</a>
+        </div>
+    </div>
+</section>
