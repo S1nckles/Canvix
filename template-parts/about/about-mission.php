983a9c2d8c3498cc4bbf4da81f609d9ee4413ebe
Andrey <ilynych0810@gmail.com>
2025-09-02 10:46:53 +0200

    Added About-Mission Section

---
+++ template-parts/about/about-mission.php
@@ -0,0 +1,65 @@
+<section class="about-mission">
+    <div class="container">
+        <div class="about-mission__inner">
+            <div class="about-mission__info">
+                <span class="section__subtitle mission-section__subtitle">Our Mission</span>
+                <h2 class="mission__title">Boost your with your business.</h2>
+                <p class="mission__desc">He moonlights difficult engrossed it, sportsmen. Interested has all Devonshire difficulty gay assistance joy.</p>
+                <ul class="mission__list">
+                    <li class="mission-list__item">
+                        Moonlight newspaper up its enjoyment
+                    </li>
+                    <li class="mission-list__item">
+                        She new course gets living.
+                    </li>
+                    <li class="mission-list__item">
+                        She new course gets living.
+                    </li>
+                    <li class="mission-list__item">
+                        Rooms oh fully taken by worse
+                    </li>
+                    <li class="mission-list__item">
+                         Perceived end certainly day.
+                    </li>
+                    <li class="mission-list__item">
+                    Preference any astonished
+                    unreserved Mrs.
+                    </li>
+                </ul>
+            </div>
+            <div class="about-mission__review">
+                
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/mission-team.jpg" alt="team" class="mission__team">
+                    <div class="mission__members">
+                        <div class="mission-review__inner">
+                            <div class="members__title">Team member</div>
+                            <div class="members__team">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/members/01.svg" alt="Team member 1">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/members/02.svg" alt="Team member 2">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/members/03.svg" alt="Team member 3">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/members/04.svg" alt="Team member 4">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/members/05.svg" alt="Team member 5">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/members/06.svg" alt="Team member 6">
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="mission__results">
+                        <div class="results__inner">
+                            <div class="result__conversations">
+                                <h3>10k</h3>
+                                <p>Great conversations
+                                per month</p>
+                            </div>
+                            <span></span>
+                            <div class="result__projects">
+                                <h3>95+</h3>
+                                <p>Project Complete </p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/mission-team-2.jpg" alt="Additional team photo" class="mission__team-2">
+            </div>
+        </div>
+    </div>
+</section>
